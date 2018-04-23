4a3439b5a2605ad0be15dddbab600e6299c2c481
zhoupaopao <1312667058@qq.com>
2018-04-23 21:05:02 +0800

    第一次尝试用git进行版本管理

---
+++ app/src/main/java/com/hivee2/mvp/ui/GitActivity.java
@@ -0,0 +1,8 @@
+package com.hivee2.mvp.ui;
+
+/**
+ * Created by 13126 on 2018/4/23.
+ */
+
+public class GitActivity {
+}
--- app/src/main/java/com/hivee2/mvp/ui/WirelessActivity.java
+++ app/src/main/java/com/hivee2/mvp/ui/WirelessActivity.java
@@ -174,6 +174,8 @@ public class WirelessActivity extends Activity implements HttpCycleContext {
         final Calendar ca = Calendar.getInstance();
         mHour = ca.get(Calendar.HOUR_OF_DAY);
         mMinutes=ca.get(Calendar.MINUTE);
+        Log.i("test git1", "测试git版本控制用");
+        Log.i("test git2", "测试git版本控制用");
     }
     @Override
     protected Dialog onCreateDialog(int id) {
